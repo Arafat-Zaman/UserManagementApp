cfa8e82ca154b18bf9843321ef0f0ec9f643023e
Arafat Zaman Anik <arafat.zaman.anik@gmail.com>
2025-01-21 19:28:06 +0600

    Usermanagement app- E2E Testing project- xUnit.

---
+++ UserManagementApp.E2ETests/E2ETestBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace UserManagementApp.E2ETests
+{
+    public class E2ETestBase : IDisposable
+    {
+        protected IWebDriver Driver;
+
+        public E2ETestBase()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            options.AddArgument("--headless"); // Optional: Run tests in headless mode
+            Driver = new ChromeDriver(options);
+
+            // Set timeouts for element searching
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        public void Dispose()
+        {
+            Driver.Quit();
+            Driver.Dispose();
+        }
+    }
+}
---
+++ UserManagementApp.E2ETests/UserManagementTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using Xunit;
+
+namespace UserManagementApp.E2ETests
+{
+    public class UserManagementTests : E2ETestBase
+    {
+        [Fact]
+        public void Should_LoadHomePage()
+        {
+            // Navigate to the application
+            Driver.Navigate().GoToUrl("http://localhost:4200");
+
+            // Assert the page title or header
+            var header = Driver.FindElement(By.TagName("h1")).Text;
+            Assert.Equal("User List", header);
+        }
+
+        [Fact]
+        public void Should_SearchUsers()
+        {
+            // Navigate to the application
+            Driver.Navigate().GoToUrl("http://localhost:4200");
+
+            // Input a search query
+            var searchBox = Driver.FindElement(By.CssSelector("input[placeholder='Search users by name, company, or role']"));
+            searchBox.SendKeys("Manager");
+
+            // Click the search button
+            var searchButton = Driver.FindElement(By.CssSelector("button"));
+            searchButton.Click();
+
+            // Assert search results
+            var rows = Driver.FindElements(By.CssSelector("table tbody tr"));
+            Assert.True(rows.Count > 0, "No users found for the search query.");
+        }
+
+        [Fact]
+        public void Should_ViewUserDetails()
+        {
+            // Navigate to the application
+            Driver.Navigate().GoToUrl("http://localhost:4200");
+
+            // Click on the first "View" link
+            var viewLink = Driver.FindElement(By.LinkText("View"));
+            viewLink.Click();
+
+            // Assert user details are displayed
+            var detailsHeader = Driver.FindElement(By.TagName("h2")).Text;
+            Assert.NotEmpty(detailsHeader);
+        }
+
+    }
+}
